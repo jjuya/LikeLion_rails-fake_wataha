c005b4ea665ae641b1b85a4ddb67e191bc1cbe19
jjuya <jjuya9381@naver.com>
2017-11-22 02:34:56 +0000

    heroku deploy ready

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  # 모든 서비스 사용 전에 로그인하세요.
+  before_action :authenticate_user!
+end
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+
+  def admin?
+    if role == "admin" #user class 여서 role로 바로 접근 가능
+      true
+    else
+      false
+    end
+  end
+end
